7c2088b0e0f9269d761ff8408ece15f01aa4347f
hdecache99 <107233094+hdecache99@users.noreply.github.com>
2023-01-25 20:40:04 -0600

    Commit15

--- operaciones.js
+++ operaciones.js
@@ -5,7 +5,6 @@ function suma(a, b) {
     return a + b;
 <<<<<<< HEAD
 }
-
 // Desarrollador 3
 }
 // Desarrollador 3
@@ -22,36 +21,24 @@ function division(a, b) {
     return a / b;
 }
 // Desarrollador 1 
-
 function potencia(base, exponente) {
-
     return Math.pow(base, exponente);
-
 }
-
 // Desarrollador 2
 function raizCuadrada(n) {
     return Math.sqrt(n);
 }
 // Desarrollador 4 
-
 function porcentaje(n, porcentaje) {
-
     return (n * porcentaje) / 100;
-
 }
 // Desarrollador 1 
 
 function promedio(numeros) {
-
     let suma = 0;
-
     for (let i = 0; i < numeros.length; i++) {
-
         suma += numeros[i];
-
     }
-
     return suma / numeros.length;
 }
 // Desarrollador 4
@@ -63,25 +50,15 @@ function factorial(n) {
     return resultado;
 }
 // Desarrollador 3 
-
 function maximo(numeros) {
-
     let max = numeros[0];
-
     for (let i = 1; i < numeros.length; i++) {
-
         if (numeros[i] > max) {
-
             max = numeros[i];
-
         }
-
     }
-
     return max;
-
 }
-
 // Desarrollador 2
 function minimo(numeros) {
     let min = numeros[0];
@@ -92,7 +69,6 @@ function minimo(numeros) {
     }
     return min;
 }
-
 // Desarrollador 3 
 console.log(suma(2, 3));  
 console.log(resta(5, 3));  
@@ -104,11 +80,10 @@ console.log(potencia(2, 3));
 console.log(raizCuadrada(9));
 
 // Desarrollador 1 
-
- 
-
 console.log(porcentaje(100, 20)); 
-
- 
-
 console.log(promedio([2, 3, 5, 7, 11])); 
+
+// Desarrollador 3 
+console.log(factorial(5)); 
+console.log(maximo([2, 3, 5, 7, 11])); 
+
