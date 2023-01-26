b06e87e13e876c353ee0d6ac695a648b15905a68
Douvasq <123609174+Douvasq@users.noreply.github.com>
2023-01-25 20:29:08 -0600

    commit 9

--- operaciones.js
+++ operaciones.js
@@ -55,3 +55,11 @@ function promedio(numeros) {
 
   return suma / numeros.length; 
   } 
+// Desarrollador 4
+function factorial(n) {
+    let resultado = 1;
+    for (let i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+} 
